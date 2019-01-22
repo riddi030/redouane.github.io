0298e1fdc2ded312a27d14689d47292c706656b7
riddi030 <46928447+riddi030@users.noreply.github.com>
2019-01-22 14:55:26 +0100

    first upload

---
+++ php/formule.php
@@ -0,0 +1,38 @@
+<?php
+$var1=11;
+$var2=19;
+
+$uitkomst= $var1+$var2;
+echo $var1 . " + " . $var2 . " = " . $uitkomst;
+echo "<br>";
+echo "<br>";
+
+
+
+$var3=20;
+$var4=19;
+
+$uitkomst2= $var3-$var4;
+echo $var3 . " - " . $var4 . " = " . $uitkomst2;
+echo "<br>";
+echo "<br>";
+
+
+
+$var5=12;
+$var6=2;
+
+$uitkomst3= $var5/$var6;
+echo $var5 . " : " . $var6 . " = " . $uitkomst3;
+echo "<br>";
+echo "<br>";
+
+
+
+$var7=23;
+$var8=3;
+
+$uitkomst4= $var7*$var8;
+echo $var7 . " x " . $var8 . " = " . $uitkomst4;
+
+?>
